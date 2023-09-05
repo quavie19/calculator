71a3532e3e70e01048805156c2c990aae6e73273
Jaquavia <jaquaviawalker@gmail.com>
2023-09-05 18:06:11 -0400

    more js edits

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
+class Calculator{
+    // construction takes all inputs and functiosn for calculator 
+    constructor(previousOperandTextElement, currentOperandTextElement) { 
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+        
+    }
+
+    delete() {
+        
+    }
+    appendNumber(number) {
+        // appends numbers together when entered in calculator . click number and adds to screen
+    }
+    chooseOperation(operation) {
+        
+    }
+    compute() {
+        
+    }
+    updateDisplay(){
+    
+    }
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement= document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
